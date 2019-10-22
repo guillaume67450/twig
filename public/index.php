6d094f4bdfc7f6bc0feed5b338829f5cb4a84b11
guillaume67450 <guillaume_d_mundo@hotmail.com>
2019-10-22 14:12:45 +0200

    correction du include de trop

--- public/index.php
+++ public/index.php
@@ -3,11 +3,11 @@ use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
 require_once __DIR__ ."/../vendor/autoload.php";
-include '../src/view/index.html.twig';
+
 
 $products = ['product1', 'product2', 'product3', 'product4', 'product5'];
 
-$loader = new Twig_Loader_Filesystem(__DIR__.'/../src/View/');
+$loader = new Twig_Loader_Filesystem(__DIR__.'/../src/view/');
 $twig = new Twig_Environment($loader);
 
 echo $twig->render('index.html.twig', ['products' => $products]);
